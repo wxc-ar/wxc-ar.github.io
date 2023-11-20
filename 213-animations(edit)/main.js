067c9c2755f9f62756b0f3bc6aad8d5ff52f379f
Xuchuan Wang <wxc@XuchuantekiMacBook-Air.local>
2023-11-19 23:13:09 -0800

    new

---
+++ 213-animations(edit)/main.js
@@ -0,0 +1,62 @@
+//import {loadGLTF} from "../../libs/loader.js";
+import {loadGLTF, loadAudio} from "../../libs/loader.js";
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: '../../assets/targets/musicband.mind',
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
+
+    const gltf = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf'); //load model
+    gltf.scene.scale.set(0.1, 0.1, 0.1);
+    gltf.scene.position.set(0, -0.4, 0);
+
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(gltf.scene);
+
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+    const action = mixer.clipAction(gltf.animations[0]);
+    
+
+    const clock = new THREE.Clock();
+
+
+    const audioClip = await loadAudio('../../assets/sounds/musicband-background.mp3');
+
+    const listener = new THREE.AudioListener();
+    camera.add(listener);
+
+    const audio = new THREE.PositionalAudio(listener);
+    anchor.group.add(audio);
+
+    audio.setBuffer(audioClip);
+    audio.setRefDistance(100);
+    audio.setLoop(true);
+
+    anchor.onTargetFound = () => {
+      audio.play();
+      action.play();
+    }
+    anchor.onTargetLost = () => {
+      audio.pause();
+    }
+
+
+    
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      const delta = clock.getDelta();
+      //gltf.scene.rotation.set(0, gltf.scene.rotation.y+delta, 0);
+      mixer.update(delta);
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
